a6df2ba94091db9693de7652935e587e0f7aa919
chris <cmayer153@gmail.com>
2021-01-14 22:41:25 -0700

    sketchy materialUI picture list, still have some toying to do

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PhotoTiles from './PhotoTiles.jsx';
+const axios = require('axios');
 
 
 class Marquee extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      displayRecord: ['exterior1', 'interior2', 'interior3']
     }
+
+    this.getListingFromServer = this.getListingFromServer.bind(this);
+  }
+
+  getListingFromServer() {
+    axios.get('/listing/44')
+      .then((response) => {
+        console.log('received listing from server: ', response);
+        this.setState({displayRecord: response.data})
+      })
+      .catch((error) => {
+        console.log('error fetching listing from server', error);
+      });
+  }
+
+  componentDidMount() {
+    //this.getListingFromServer();
   }
 
   render() {
       return (
-          <div>MARQUEE MOON</div>
+          <div>
+            <PhotoTiles listing={this.state.displayRecord} />
+            
+          </div>
       );
   }
 }
