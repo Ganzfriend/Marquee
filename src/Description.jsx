be4c0e5ca6896d4b486f1a02488bdfe7e91b5b36
chris <cmayer153@gmail.com>
2021-01-23 13:51:28 -0700

    sketched out sleeping arrangements.

--- src/Description.jsx
+++ src/Description.jsx
@@ -2,14 +2,41 @@ import React from 'react';
 import Modal from '@material-ui/core/Modal';
 
 var Description = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const modalBody = (
+    <div>
+      <p>
+        {props.listing ? props.listing.description : '...'}
+      </p>
+    </div>
+  );
+  
+
   if ( props.listing === null) {
     return null;
   } else {
     return (
-      <p>{props.listing.description.substr(0, 200)}
-        ...
-        <span>read more</span>
-      </p>
+      <div>
+        <p>{props.listing.description.substr(0, 200)}
+          ...
+          <span onClick={handleOpen}>read more</span>
+        </p>
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          {modalBody}
+        </Modal>
+      </div>
     )
   }
 }
--- src/Sleeping.jsx
+++ src/Sleeping.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import './fashion.css';
+
+
 
 var Sleeping = (props) => {
   if ( props.listing === null) {
     return null;
   } else {
+    var roomNum = 1;
     return (
-      <p> {props.listing.description} </p>
+      <div>
+        <h2>Sleeping arrangements</h2>
+        {props.listing.bedrooms.map( (bedroom) => (
+          <div className="marquee-sleeping-span">
+            <h3>Bedroom {roomNum++}</h3>
+            <h6>{bedroom}</h6>
+          </div>
+        ))}
+      </div>
     )
   }
 }
