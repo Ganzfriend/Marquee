d10089f44593ffbe0d4715cd8a4841641dd41549
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-01-30 11:45:05 -0700

    add localhost option to mongo uri

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN mkdir -p /marquee/app
 WORKDIR /marquee/app
 COPY . /marquee/app
 RUN npm install
+ENV MONGODBURL='mongodb://database/marquee'
 EXPOSE 3004
 CMD [ "npm", "start" ]
--- database/index.js
+++ database/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-//const mongoURI = 'mongodb://localhost/marquee_4';
-//const mongoURI = 'mongodb://0.0.0.0:3027/marquee';
-const mongoURI = 'mongodb://database/marquee';
 
-const db = mongoose.connect(mongoURI);
+const mongoURI = process.env.MONGODBURL || 'mongodb://localhost/marquee';
+
+const db = mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 module.exports = db;
